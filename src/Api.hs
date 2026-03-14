4b01b97e9c6f7e34668bbbc690aea30ce765e121
MWK <marc@budabe.eu>
2026-03-14 14:25:58 +0100

    Corrected number of open reviews

--- src/Api.hs
+++ src/Api.hs
@@ -13,16 +13,15 @@ module Api
 
 import Control.Exception (Exception)
 import Data.Aeson (FromJSON(..), (.:), withObject)
--- import qualified Data.Aeson as Aeson
+import Data.Time (UTCTime(..), addUTCTime)
+import Data.Time.Format.ISO8601 (iso8601ParseM)
+
 import qualified Data.ByteString.Char8 as BS8
+import Data.List (sortOn)
 import Data.Text (Text)
 import qualified Data.Text as T
+
 import Network.HTTP.Req
-import Data.List (sortOn, foldl')
-import Data.Time (getCurrentTime, getCurrentTimeZone, utcToLocalTime)
-import Data.Time (UTCTime(..), addUTCTime, utctDay, utctDayTime)
-import Data.Time.Format (defaultTimeLocale, formatTime)
-import Data.Time.Format.ISO8601 (iso8601ParseM)
 
 -- A small "domain" type used by Main.hs
 data User = User
@@ -31,7 +30,7 @@ data User = User
   , userProfileUrl :: String
   } deriving (Show, Eq)
 
--- WaniKani wraps the actual user record in a top-level "data" field.
+-- WaniKani wraps the actual record in a top-level "data" field.
 newtype UserEnvelope = UserEnvelope
   { ueData :: UserData
   } deriving (Show)
@@ -53,16 +52,19 @@ instance FromJSON UserData where
       <*> o .: "level"
       <*> o .: "profile_url"
 
+-- Summary endpoint types
 data Summary = Summary
   { summaryReviews :: [ReviewBucket]
-  } deriving (Show)
+  } deriving (Show, Eq)
 
 data ReviewBucket = ReviewBucket
   { rbAvailableAt :: UTCTime
   , rbSubjectIds  :: [Int]
-  } deriving (Show)
+  } deriving (Show, Eq)
 
-newtype SummaryEnvelope = SummaryEnvelope { seData :: Summary } deriving (Show)
+newtype SummaryEnvelope = SummaryEnvelope
+  { seData :: Summary
+  } deriving (Show)
 
 instance FromJSON SummaryEnvelope where
   parseJSON = withObject "SummaryEnvelope" $ \o ->
@@ -85,6 +87,7 @@ data KrokiError
 
 instance Exception KrokiError
 
+-- API calls
 getUser :: String -> IO User
 getUser token = runReq defaultHttpConfig $ do
   let authHeader = header "Authorization" ("Bearer " <> BS8.pack token)
@@ -110,60 +113,70 @@ getSummary :: String -> IO Summary
 getSummary token = runReq defaultHttpConfig $ do
   let authHeader = header "Authorization" ("Bearer " <> BS8.pack token)
       revHeader  = header "Wanikani-Revision" "20170710"
+
   resp <- req
     GET
     (https "api.wanikani.com" /: "v2" /: "summary")
     NoReqBody
     jsonResponse
     (authHeader <> revHeader)
+
   let env = responseBody resp :: SummaryEnvelope
   pure (seData env)
 
+-- Review helpers
 reviewsAvailableNow :: UTCTime -> Summary -> Int
 reviewsAvailableNow now s =
-    sum [ length (rbSubjectIds b) | b <- summaryReviews s, rbAvailableAt b <= now ]
+  sum [ length (rbSubjectIds b)
+      | b <- summaryReviews s
+      , rbAvailableAt b <= now
+      ]
 
 nextReviewBucket :: UTCTime -> Summary -> Maybe (UTCTime, Int)
 nextReviewBucket now s =
-  case sortOn rbAvailableAt [ b | b <- summaryReviews s, rbAvailableAt b > now, not (null (rbSubjectIds b)) ] of
+  case sortOn rbAvailableAt
+        [ b | b <- summaryReviews s
+            , rbAvailableAt b > now
+            , not (null (rbSubjectIds b))
+        ] of
     (b:_) -> Just (rbAvailableAt b, length (rbSubjectIds b))
     []    -> Nothing
 
--- Truncate a UTCTime down to the start of the hour (UTC)
-floorToHourUTC :: UTCTime -> UTCTime
-floorToHourUTC t =
-  let day    = utctDay t
-      secs   = floor (utctDayTime t) :: Int
-      secs'  = (secs `div` 3600) * 3600
-  in UTCTime day (fromIntegral secs')
-
-countBucket :: ReviewBucket -> Int
-countBucket = length . rbSubjectIds
-
--- Number of reviews that become available in [start, end)
-newBetween :: UTCTime -> UTCTime -> Summary -> Int
-newBetween start end s =
-  sum [ countBucket b
-      | b <- summaryReviews s
-      , rbAvailableAt b >= start
-      , rbAvailableAt b <  end
-      ]
-
--- Total reviews available up to time 't' (i.e., available_at <= t)
-openUpTo :: UTCTime -> Summary -> Int
-openUpTo t s =
-  sum [ countBucket b
+-- Projection semantics:
+-- Row 0 (timestamp = now): new == open == number currently available.
+-- Rows 1..23: for each next hour window, "new" is what becomes available
+-- in that hour; "open" is cumulative from now (assuming you do no reviews).
+openAt :: UTCTime -> Summary -> Int
+openAt t s =
+  sum [ length (rbSubjectIds b)
       | b <- summaryReviews s
       , rbAvailableAt b <= t
       ]
--- (hourStartUTC, newInThatHour, openAtEndOfHour)
+
+newInWindow :: UTCTime -> UTCTime -> Summary -> Int
+newInWindow start end s =
+  sum [ length (rbSubjectIds b)
+      | b <- summaryReviews s
+      , rbAvailableAt b >  start
+      , rbAvailableAt b <= end
+      ]
+
 reviewsPerHourNext24 :: UTCTime -> Summary -> [(UTCTime, Int, Int)]
 reviewsPerHourNext24 now s =
-  let h0 = floorToHourUTC now
+  let openNow = openAt now s
+
+      mk :: Int -> (UTCTime, Int, Int)
+      mk 0 = (now, openNow, openNow)
       mk i =
-        let start = addUTCTime (fromIntegral (i * 3600)) h0
+        let start = addUTCTime (fromIntegral ((i - 1) * 3600)) now
             end   = addUTCTime 3600 start
-            newN  = newBetween start end s
-            openN = openUpTo end s
+            newN  = newInWindow start end s
+            openN = openNow + sum
+                      [ newInWindow (addUTCTime (fromIntegral ((j - 1) * 3600)) now)
+                                    (addUTCTime (fromIntegral (j * 3600)) now)
+                                    s
+                      | j <- [1..i]
+                      ]
         in (start, newN, openN)
+
   in map mk ([0..23] :: [Int])
